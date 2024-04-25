8ebabe8a7d09c33e67097207ae88f758921602d7
RatingRishu <rairishabh5001@gmail.com>
2024-04-25 13:55:18 +0530

    Project Update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  showMore: boolean = false;
+  // showMore: boolean = false;
+  showMoreGoogle = false;
+  showMoreApna = false;
+  showMoreWeather = false;
+  showMoreCarbike = false;
+  showMoreCarLoan = false;
 
-  toggleMore() {
-    this.showMore = !this.showMore;
+  toggleMore(section: string) {
+    if (section === 'google') {
+      this.showMoreGoogle = !this.showMoreGoogle;
+    } else if (section === 'apna') {
+      this.showMoreApna = !this.showMoreApna;
+    }
+    else if (section === 'weather') {
+      this.showMoreWeather = !this.showMoreWeather;
+    }
+    else if (section === 'carbike') {
+      this.showMoreCarbike = !this.showMoreCarbike;
+    }
+    else if (section === 'carloan') {
+      this.showMoreCarLoan = !this.showMoreCarLoan;
+    }
   }
-
 }
+
+
