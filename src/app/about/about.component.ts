93b2b53d5ca82056e04d8f7a9aed9dff3eb87823
RatingRishu <rairishabh5001@gmail.com>
2024-07-10 10:28:20 +0530

    Annimation Setup

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
 
+    document.querySelectorAll('.skill').forEach(skill => {
+      observer.observe(skill);
+    });
+  }
 }
